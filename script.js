8acc1c71cf461582f9c55498a08066c3b5d6d0a6
danielaosorio <daniela.osorio01@uptc.edu.co>
2026-09-12 16:23:48 -0500

    Agrego parte del codigo script

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const navToggle = document.getElementById('navToggle');
 const nav = document.getElementById('nav');
+document.addEventListener("DOMContentLoaded", () => {
+    const botones = document.querySelectorAll(".btn--primary, button");
+
+    botones.forEach(boton => {
+        boton.addEventListener("click", function(e) {
+            // Efecto visual rápido de escala al hacer clic
+            this.style.transform = "scale(0.95)";
+            setTimeout(() => {
+                this.style.transform = "translateY(-4px) scale(1.02)";
+            }, 150);
+        });
+    });
+});
