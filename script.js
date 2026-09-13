3deeea1f60139cb81bcdd352d1e70cc8e248562a
LMayxz <149394095+LMayxz@users.noreply.github.com>
2026-09-13 17:35:05 +0000

    feat: implementacion completa de cabecera, hero section y estilos del prototip, LN

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Sistema AutoFix Pro inicializado por Grupo 3");
+console.log("Sistema AutoFix inicializado por Grupo 3");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btnLogin = document.getElementById("btnLogin");
+    const btnRegister = document.getElementById("btnRegister");
+
+    if (btnLogin) {
+        btnLogin.addEventListener("click", () => {
+            alert("El inicio de sesión está en desarrollo.");
+        });
+    }
+
+    if (btnRegister) {
+        btnRegister.addEventListener("click", () => {
+            alert("El registro está en desarrollo.");
+        });
+    }
+
+});
